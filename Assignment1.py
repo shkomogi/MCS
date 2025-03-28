c4de01e7d927fde72d2dcfaa8f70e8db73d93202
Shirley Komogi <88008991+shkomogi@users.noreply.github.com>
2025-03-28 10:01:56 +1000

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,49 @@
+"""Assignment 1 - MCS253
+Creating Python Objects ~ Lists, Tuples, Dictionaries"""
+
+#Due Date - Submit no later than 4:10 pm on Thursday (3rd April 2025)
+
+#Please type in your name in the next line by way of commenting
+
+#Name: ___________
+
+#----------------------------------------------------------------
+
+#Quesiton 1
+
+"""Create a python object to store all the courses you are taking this semester.
+You may be required to change a course next week."""
+
+#Question 2
+
+"""Store all MCS 253 students ID in a python object. These IDs will be
+used for 4 years thus do not change IDs."""
+
+#Question 3
+"""Since students IDs are a unique identifying number for each student, 
+
+use it as a reference to point to students records. The students records
+should consist of full names, gender, DOB,Province of origin & email addresses."""
+
+#Question 4
+"""Using indexing, print out your favorite course for the sem (refer to Q1)"""
+
+#Question 5
+"""Print out only your student ID from Q2"""
+
+#Question 6
+"""Print out all students records (exclude the IDs)"""
+
+#Question 7
+"""Print out only your student record"""
+
+#Question 8
+"""Update the student ID in Q2.
+What is the error here?
+WHy do you think this happened"""
+
+#Question 9
+"""Print out the data types for all the objects you have created so far"""
+
+#------end of Assignment
+#UPLOAD your completed A1 to your GitHub account. Only share the link with me. 
